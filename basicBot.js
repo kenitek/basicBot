fc30e9f3ff742a69e64eca25536132ed9e5819ed
kenitek <adam.p.kwong@gmail.com>
2015-02-02 20:13:13 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1591,27 +1591,14 @@
                     'has given you a cookie for Goris. Is he here? :minik:',
                     'gives you a fortune cookie. It reads "1v1 me for the boss of this gym."',
                     'gives you a cookie made of plebs. Cookie is still grey. :sadness:',
-                    'gives you a fortune cookie. It reads "Roses are red, Violets are blue, Sugar is sweet, AND HIS NAME IS JOHN CEEEENAAAAAAAAAAAAAA!!!" :swiftrage:',
-                    'has given you a chocolate chip cookie!',
-                    'has given you a soft homemade oatmeal cookie!',
-                    'has given you a plain, dry, old cookie. It was the last one in the bag. Gross.',
-                    'gives you a sugar cookie. What, no frosting and sprinkles? 0/10 would not touch.',
-                    'gives you a chocolate chip cookie. Oh wait, those are raisins. Bleck!',
-                    'gives you an enormous cookie. Poking it gives you more cookies. Weird.',
-                    'gives you a fortune cookie. It reads "Why aren\'t you working on any projects?"',
-                    'gives you a fortune cookie. It reads "Give that special someone a compliment"',
-                    'gives you a fortune cookie. It reads "Take a risk!"',
-                    'gives you a fortune cookie. It reads "Go outside."',
-                    'gives you a fortune cookie. It reads "Don\'t forget to eat your veggies!"',
-                    'gives you a fortune cookie. It reads "Do you even lift?"',
-                    'gives you a fortune cookie. It reads "m808 pls"',
-                    'gives you a fortune cookie. It reads "If you move your hips, you\'ll get all the ladies."',
-                    'gives you a fortune cookie. It reads "I love you."',
-                    'gives you a Golden Cookie. You can\'t eat it because it is made of gold. Dammit.',
-                    'gives you an Oreo cookie with a glass of milk!',
-                    'gives you a rainbow cookie made with love :heart:',
-                    'gives you an old cookie that was left out in the rain, it\'s moldy.',
-                    'bakes you fresh cookies, it smells amazing.'
+                    'gives you a fortune cookie. It reads "AND HIS NAME IS JOHN CEEEENAAAAAAAAAAAAAA!!!" :PogChamp:',
+                    'gives you a cookie. it reads "sex me"',
+                    'bakes a cookie, it\'s for Darude :canyounot::kolentosniperino:',
+                    'gives you a cookie. It reads :wuotm8:',
+                    'gives you a cookie. it reads "rip :nipaface:"',
+                    'gives you a cookie. It reads "1v1 me rust"',
+                    'gives you a cookie. It got taken away by the Nazis :forsensheffy:7',
+                    'gives you... HornPub* a cookie :biblethump:',
                 ],
                 getCookie: function () {
                     var c = Math.floor(Math.random() * 5);
@@ -2188,7 +2175,7 @@
 
             maxlengthCommand: {
                 command: 'maxlength',
-                rank: 'manager',
+                rank: 'bouncer',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
@@ -2630,7 +2617,7 @@
 
             swapCommand: {
                 command: 'swap',
-                rank: 'mod',
+                rank: 'bouncer',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
@@ -2741,7 +2728,7 @@
 
             unbanCommand: {
                 command: 'unban',
-                rank: 'bouncer',
+                rank: 'manager',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
@@ -2792,7 +2779,7 @@
 
             unmuteCommand: {
                 command: 'unmute',
-                rank: 'bouncer',
+                rank: 'manager',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
