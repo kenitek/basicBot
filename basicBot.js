2cd53f09c8b3ee2f222d86b00947d79889cf41ab
kenitek <adam.p.kwong@gmail.com>
2015-02-02 20:20:36 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2941,6 +2941,19 @@
             }
         }
     };
+    
+        baconCommand = {
+            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Bacon!!!");
+                }
+            }
+        };
 
     loadChat(basicBot.startup);
 }).call(this);
